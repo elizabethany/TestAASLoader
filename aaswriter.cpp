f99cfac0e17850f44a2140392a77527a3954816f
beth <elizabethany1312@gmail.com>
2022-02-07 15:57:33 -0500

    added console input for inputs and ouputs + misc code for testing

--- aaswriter.cpp
+++ aaswriter.cpp
@@ -2,4 +2,8 @@
 #include "aas.hpp"
 
 void write_aas(idAAS2File* outdata, const char* tofile) {
+	FILE* f = nullptr;
+	fopen_s(&f, tofile, "a");
+	fputs("test", f);
+	fclose(f);
 }
--- testmain.cpp
+++ testmain.cpp
@@ -2,11 +2,25 @@
 #include <iostream>
 #include "aas.hpp"
 
+char* getFilePath()
+{
+	char* filePath = new char[150];
+	std::cin.getline(filePath, 150);
 
-int main(int argc, const char** argv) {
-	
-	idAAS2File myfile{};
-	load_aas(&myfile, argv[1]);
+	return filePath;
+}
+
+int main() {
+
+	idAAS2File myFile{};
+
+	std::cout << ".BAAS file path: ";
+	const char* aasFilePath = getFilePath();
+	load_aas(&myFile, aasFilePath);
+
+	std::cout << "Output file path: ";
+	const char* outputPath = getFilePath();
+	write_aas(&myFile, outputPath);
 
 	return 0;
 
